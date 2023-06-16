5913a274906e1ca05b9c835b35fbd1cdb22c11b4
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-06-16 05:06:37 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'item_genres/index'
-  get 'item_genres/edit'
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
