dc8d35e8dfa48dd07869ea61fcf2137da51f883a
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-16 08:20:54 +0000

    [save]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,9 +1,13 @@
 class Admin::ItemsController < ApplicationController
 
   def new
+    @item = item.new
   end
   
   def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to 'admin_item_path(item)'
     
   end
   
@@ -18,6 +22,13 @@ class Admin::ItemsController < ApplicationController
   end
   
   def update
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :genre_id, :introduction, :price, :is_availabled, :item_image)
+    
   end
   
 end
