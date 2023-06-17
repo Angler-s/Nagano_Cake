d87d01b3f6c6c461a6f4dabd83c3fbce5b7675f4
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-17 01:49:04 +0000

    [update]app/views/admin/items/new

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,10 +12,10 @@ class Item < ApplicationRecord
   validates :is_availabled, presence: true
 
   def get_item_image(width, heigh)
-    unless item_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      item_image.attach(io: File.open(file_path), filename: 'default-image-jpg', content_type: 'image/jpeg')
-    end
+    # unless item_image.attached?
+    #   file_path = Rails.root.join('app/assets/images/no_image.jpg')
+    #   item_image.attach(io: File.open(file_path), filename: 'default-image-jpg', content_type: 'image/jpeg')
+    # end
     item_image.variant(resize_to_limit: [width, heigh]).processed
   end
 
