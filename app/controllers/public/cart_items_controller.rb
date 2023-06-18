15b4f71717d12ab680579b249fe0e932fea5d5e6
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-18 07:42:53 +0000

    [save]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,43 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items.all 
   end
+  
+  def create
+    customer = current_customer
+    item = Item.find(params[:cart_item][:item_id])
+    cart_item_new = customer.cart_items.new(cart_item_params)
+    if CartItem.find_by(item_id:item.id, customer_id:customer ).present?
+      cart_item = CartItem.find_by(item_id:item, customer_id:customer )
+      cart_item.count += params[:cart_item][:count].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    else cart_item_new.save
+      redirect_to cart_items_path
+    end
+  end
+  
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
+
+  end
+  
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    @cart_items = CartItem.all
+  end
+  
+  def destroy_all
+    cart_items = CartItem.all
+    cart_items.destroy
+    @cart_items = CartItem.all
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :count)
+  end
+
 end
