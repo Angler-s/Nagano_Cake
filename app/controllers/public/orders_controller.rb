17e640a8dd91138f99627d05385f7f28f024c24d
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-06-18 07:06:35 +0000

    [add]gem enum_helper[update]order/complete,new画面を更新

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,14 +1,21 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @customer = current_customer
   end
 
   def index
+    @customer = current_customer
+    @orders = @customer.orders
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
 
   def confirm
+    @order_items
   end
 
   def complete
