f8f96174565ed3aee1e4c9481909be6e0b9cd779
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-06-18 09:49:26 +0000

    [fixed]controller,view

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,8 +1,9 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = current_customer.cart_items.all 
+    @cart_items = current_customer.cart_items.all
+    @total = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
-  
+
   def create
     customer = current_customer
     item = Item.find(params[:cart_item][:item_id])
@@ -16,28 +17,28 @@ class Public::CartItemsController < ApplicationController
       redirect_to cart_items_path
     end
   end
-  
+
   def update
     cart_item = CartItem.find(params[:id])
     cart_item.update(cart_item_params)
     redirect_to cart_items_path
 
   end
-  
+
   def destroy
     cart_item = CartItem.find(params[:id])
     cart_item.destroy
     @cart_items = CartItem.all
     redirect_to cart_items_path
   end
-  
+
   def destroy_all
     cart_items = CartItem.all
     cart_items.destroy_all
     @cart_items = CartItem.all
     redirect_to cart_items_path
   end
-  
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:customer_id, :item_id, :count)
