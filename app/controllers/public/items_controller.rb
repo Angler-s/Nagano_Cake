dbefebdb6a65121061b50606b4685733ba70a904
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-06-18 06:50:54 +0000

    [add view]cart_item,public_item

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,5 @@
 class Public::ItemsController < ApplicationController
+  
   def index
     @genres = ItemGenre.all
     @items = Item.page(params[:page])
@@ -7,5 +8,7 @@ class Public::ItemsController < ApplicationController
   def show
     @genres = ItemGenre.all
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
+
 end
