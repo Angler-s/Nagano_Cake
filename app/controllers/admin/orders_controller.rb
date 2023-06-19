2fb9ab3677686c10aa0a53db494c9442a904b9b2
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-06-19 02:26:46 +0000

    [Add]注文履歴詳細画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,12 +7,12 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order_items = OrderItems.where(order_id: params[[:id]])
+    @order_items = OrderItems.where(order_id: params[[:id])
     if @order.update(order_params)
-      @order_items.update_all(status: 1) if @order.status == "payment_confirmed"
+      @order_items.update_all(status: 1) if @order.status == "confirm_payment"
     end
 
-  redirect_to admin_order_path(@order)
+    redirect_to admin_order_path(@order)
   end
 
   private
