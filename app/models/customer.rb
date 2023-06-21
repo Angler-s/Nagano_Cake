fbb421b6bb73ff08fed7e961a33ea8e5a76e44b2
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-06-21 10:02:25 +0000

    [update]注文情報入力画面にバリデーションを設定

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -14,7 +14,7 @@ class Customer < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :email,           presence: true, uniqueness: true
   validates :postal_code,     presence: true, format: { with: /\A\d{7}\z/ }
-  validates :address,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥a-zA-Z]+\z/ }
+  validates :address,         presence: true
   validates :phone_number,    presence: true, format: { with: /\A0\d{9,10}\z/}
 
   def full_name
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,9 +2,13 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_items
 
+  validates :name,          presence: true
+  validates :postal_code,   presence: true, format: { with: /\A\d{7}\z/ }
+  validates :address,       presence: true, format: { with: /\A[ぁ-んァ-ン一-龥a-zA-Z]+\z/ }
+
   enum payment: { credit_card: 0, transfer: 1 }
   enum status: { waiting_payment: 0, confirm_payment: 1, producting: 2, waiting_delivery: 3, start_delivery: 4 }
-  
+
   def subtotal
     item.with_tax_price * count
   end
