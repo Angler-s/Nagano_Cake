ef8a8ecd9c447514b98a909d197ea26a5f03bbc4
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-21 08:35:23 +0000

    [add]admin/search_function

--- app/controllers/admin/searchs_controller.rb
+++ app/controllers/admin/searchs_controller.rb
@@ -1,9 +1,21 @@
 class Admin::SearchsController < ApplicationController
   def index
 
+
+# Itemのみ検索の場合
+    # def index
+
+    #   @q = Item.ransack(params[:q])
+    #   @items = @q.result(distinct: true)
+      
+    # end
+    
     def index
-      @q = Item.ransack(params[:q])
-      @items = @q.result(distinct: true).page(params[:page]).oder(created_at desc)
+      @q = params[:q]
+      @items = Item.ransack(name_cont: @q).result
+      @customers = Customer.ransack(last_name_cont: @q).result
+
     end
+    
   end
 end
