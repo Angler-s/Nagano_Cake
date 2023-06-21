36332aa7c34b5ccbc8ab0b9f65852eccbb7a0897
ikutanaoko <132249442+ikutanaoko@users.noreply.github.com>
2023-06-21 04:42:05 +0000

    [save]

---
+++ app/controllers/admin/searchs_controller.rb
@@ -0,0 +1,9 @@
+class Admin::SearchsController < ApplicationController
+  def index
+    
+    def index
+      @q = Item.ransack(params[:q])
+      @items = @q.result(distinct: true).page(params[:page]).oder(created_at desc)
+    end
+  end
+end
