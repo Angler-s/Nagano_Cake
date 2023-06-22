62ecd4585b2c0f718739b903138679c4d825ded1
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-06-22 06:03:39 +0000

    [fix]顧客の注文履歴一覧の不具合を修正,[add]顧客一覧ページにページネーションを設置

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -6,16 +6,16 @@ class Admin::OrderItemsController < ApplicationController
     @order = @order_item.order
     @order_items = @order.order_items.all
     is_updated = true
-    
+
     if @order_item.update(order_item_params)
       @order.update(status: 2) if @order_item.status == "producting"
-      
+
       @order_items.each do |order_item|
         if order_item.status != "completed_product"
           is_updated = false
         end
       end
-      
+
       @order.update(status: 3) if is_updated
       redirect_to admin_order_path(@order)
     else
