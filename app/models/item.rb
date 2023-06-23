3b1085c32269d0bc49a962f9a8b5258c021e60d9
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-06-23 02:43:31 +0000

    [Fix]バリデーションの削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   has_many :cart_items
   has_many :order_items
 
-  validates :genre_id, presence: true
   validates :name, presence: true
   validates :introduction, presence: true
   validates :price, presence: true,
