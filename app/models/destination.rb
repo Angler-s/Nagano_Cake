d1e69ec53aa0702ceb3fa894a6c147f18e39fb0a
ryuto0111 <129473003+ryuto0111@users.noreply.github.com>
2023-06-24 09:32:49 +0000

    [Fix]コメントアウト忘れの変更/バリデーションの不足部分を追加

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,7 +2,7 @@ class Destination < ApplicationRecord
   belongs_to :customer
 
   validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
-  validates :address,     presence: true, format: { with: /\A[ぁ-んァ-ン[ ][　]一-龥a-zA-Z]+\z/ }
+  validates :address,     presence: true, format: { with: /\A[ぁ-んァ-ン[ ][　]一-龥々a-zA-Z]+\z/ }
   validates :name,        presence: true, format: { with: /\A[ぁ-んァ-ン一-龥a-zA-Z]+\z/ }
 
   def address_display
